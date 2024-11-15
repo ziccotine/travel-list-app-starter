1d032fbc3581fd37173444da18132b9865093d0c
andytaotest <andytaotest@gmail.com>
2024-11-15 13:08:06 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+// Initial packing items
+const initialItems = [
+  { id: 1, description: "Shirt", quantity: 5, packed: false },
+  { id: 2, description: "Pants", quantity: 2, packed: false },
+];
+
+function Logo() {
+  return <h1>My Travel List</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need to pack?</h3>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <li>{item.description}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items in the list. You already packed Y (Z%).</em>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
